d444a27054ad4efb6611b6c0d2ac1486aac6e6e7
Emerson Ford <emersontford@gmail.com>
2019-11-26 19:20:20 -0700

    Upping the MTU

--- main.c
+++ main.c
@@ -895,7 +895,7 @@ static int modify_qp_to_rtr(struct ibv_qp *qp, uint32_t remote_qpn, uint16_t dli
 	memset(&attr, 0, sizeof(attr));
 
 	attr.qp_state = IBV_QPS_RTR;
-	attr.path_mtu = IBV_MTU_256;
+	attr.path_mtu = IBV_MTU_2048;
 	attr.dest_qp_num = remote_qpn;
 	attr.rq_psn = 0;
 	attr.max_dest_rd_atomic = 1;
