3e142858f9304d76c8939455674b1c874500d5a5
Emerson Ford <emersontford@gmail.com>
2019-11-26 21:01:24 -0700

    Adding some more data

--- data/histogram.r
+++ data/histogram.r
@@ -1,6 +1,8 @@
 library(ggplot2)
 library(reshape2)
-file <- "trial-mtu-2048.csv"
+file <- "16384-columns-nanoseconds.csv"
+left.x.lim <- 7500
+right.x.lim <- 8500
 
 data <- read.csv(file)
 
@@ -11,4 +13,4 @@ frame <- data.frame(first_read=data[,1], second_read=data[,2])
 # plot(read1.d, xlim=c(4000, 5000))
 # polygon(read1.d, col=rgb(1, 0, 0, 0.5))
 
-ggplot(melt(frame), aes(x = value, fill = variable)) + geom_density(alpha=0.5) + xlim(3500, 4500) + labs(title=file, x="cycles", fill="read type")
+ggplot(melt(frame), aes(x = value, fill = variable)) + geom_density(alpha=0.5) + xlim(left.x.lim, right.x.lim) + labs(title=file, x="cycles", fill="read type")
