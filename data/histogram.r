bbd643a3cbff8df5575841ca85d86d152065d354
Emerson Ford <emersontford@gmail.com>
2019-11-26 20:40:56 -0700

    Adding data for 2048 MTU 1000 iteration random trial

---
+++ data/histogram.r
@@ -0,0 +1,14 @@
+library(ggplot2)
+library(reshape2)
+file <- "trial-mtu-2048-random.csv"
+
+data <- read.csv(file)
+
+# hist(data$read1, col=rgb(1, 0, 0, 0.5), xlim=c(4000, 5000), ylim=c(0, 1000), breaks=2500)
+# hist(data$read2, col=rgb(0, 0, 1, 0.5), breaks=2500, add=T)
+
+frame <- data.frame(first_read=data[,1], second_read=data[,2])
+# plot(read1.d, xlim=c(4000, 5000))
+# polygon(read1.d, col=rgb(1, 0, 0, 0.5))
+
+ggplot(melt(frame), aes(x = value, fill = variable)) + geom_density(alpha=0.5) + xlim(4000, 5000) + labs(title=file, x="cycles", fill="read type")
