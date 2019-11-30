f21c52b91c7e8dae7678f9ce944cc7d6493b7110
Emerson Ford <emersontford@gmail.com>
2019-11-29 18:33:44 -0700

    Adding more data from notchpeak

--- data/histogram.r
+++ data/histogram.r
@@ -1,18 +1,17 @@
 library(ggplot2)
 library(reshape2)
-file <- "trial-notchpeak-nanoseconds.csv"
-left.x.lim <- 3000
-right.x.lim <- 6000
-first_read_col <- 1
-second_read_col <- 2
+file <- "trial-notchpeak-ns-5.csv"
+left.x.lim <- 7500
+right.x.lim <- 10500
+first.read.col <- 3
+second.read.col <- 4
+x.label <- "nanoseconds"
 
 data <- read.csv(file)
 
-# hist(data$read1, col=rgb(1, 0, 0, 0.5), xlim=c(4000, 5000), ylim=c(0, 1000), breaks=2500)
-# hist(data$read2, col=rgb(0, 0, 1, 0.5), breaks=2500, add=T)
+frame <- data.frame(first_read=data[,first.read.col], second_read=data[,second.read.col])
 
-frame <- data.frame(first_read=data[,first_read_col], second_read=data[,second_read_col])
-# plot(read1.d, xlim=c(4000, 5000))
-# polygon(read1.d, col=rgb(1, 0, 0, 0.5))
-
-ggplot(melt(frame), aes(x = value, fill = variable)) + geom_density(alpha=0.5) + xlim(left.x.lim, right.x.lim) + labs(title=file, x="cycles", fill="read type")
+ggplot(melt(frame), aes(x = value, fill = variable)) +
+    geom_density(alpha=0.5) +
+    xlim(left.x.lim, right.x.lim) +
+    labs(title=file, x=x.label, fill="read type")
