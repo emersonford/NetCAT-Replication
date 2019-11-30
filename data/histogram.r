b17b2240555d47996f8fae6f885a939f884468d9
Emerson Ford <emersontford@gmail.com>
2019-11-29 19:02:19 -0700

    Adding rand nanosecond data

--- data/histogram.r
+++ data/histogram.r
@@ -1,14 +1,17 @@
 library(ggplot2)
 library(reshape2)
-file <- "trial-mtu-2048-random.csv"
+file <- "trial-notchpeak-ns.csv"
+left.x.lim <- 2000
+right.x.lim <- 2750
+first.read.col <- 3
+second.read.col <- 4
+x.label <- "nanoseconds"
 
 data <- read.csv(file)
 
-# hist(data$read1, col=rgb(1, 0, 0, 0.5), xlim=c(4000, 5000), ylim=c(0, 1000), breaks=2500)
-# hist(data$read2, col=rgb(0, 0, 1, 0.5), breaks=2500, add=T)
+frame <- data.frame(first_read=data[,first.read.col], second_read=data[,second.read.col])
 
-frame <- data.frame(first_read=data[,1], second_read=data[,2])
-# plot(read1.d, xlim=c(4000, 5000))
-# polygon(read1.d, col=rgb(1, 0, 0, 0.5))
-
-ggplot(melt(frame), aes(x = value, fill = variable)) + geom_density(alpha=0.5) + xlim(4000, 5000) + labs(title=file, x="cycles", fill="read type")
+ggplot(melt(frame), aes(x = value, fill = variable)) +
+    geom_density(alpha=0.5) +
+    xlim(left.x.lim, right.x.lim) +
+    labs(title=file, x=x.label, fill="read type")
